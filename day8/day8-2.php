d82f327d14d0509063b391e52b17a3846f739a03
Balage Spanyik <spanyik.balazs@gmail.com>
2023-12-09 17:24:57 +0100

    Add missing strict types declarations to Day 8

--- day8/day8-2.php
+++ day8/day8-2.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 $input = file_get_contents($argv[1] ?? 'input.txt');
 if ($input === false) {
     die('There\'s something wrong with the input file. I don\'t know what it is.' . PHP_EOL);
